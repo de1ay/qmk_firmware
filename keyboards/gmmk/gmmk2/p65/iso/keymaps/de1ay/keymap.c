9599b32748b63fe59e1cfc7472aaa9c66d58a0af
Nikita Ivanov <github@de1ay.me>
2024-12-29 22:33:10 +0300

    change additional backspace to function key

--- keyboards/gmmk/gmmk2/p65/iso/keymaps/de1ay/keymap.c
+++ keyboards/gmmk/gmmk2/p65/iso/keymaps/de1ay/keymap.c
@@ -31,11 +31,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Keymap _QL: Qwerty Layer (Default Layer)
    */
 [_QL] = LAYOUT_65_iso_blocker(
-  KC_GRAVE, KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_DEL,
-  KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,            KC_PGUP,
-  KC_ESC,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_NUHS,  KC_ENT,   KC_PGDN,
-  KC_LSFT,  KC_BSPC,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,    KC_RCTL,
-  KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_RALT,  MO(_QFL), KC_LEFT,  KC_DOWN,  KC_RGHT),
+  KC_GRAVE, KC_1,      KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_DEL,
+  KC_TAB,   KC_Q,      KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,            KC_PGUP,
+  KC_ESC,   KC_A,      KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_NUHS,  KC_ENT,   KC_PGDN,
+  KC_LSFT,  MO(_QFL),  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,    KC_RCTL,
+  KC_LCTL,  KC_LGUI,   KC_LALT,                                KC_SPC,                                 KC_RALT,  MO(_QFL), KC_LEFT,  KC_DOWN,  KC_RGHT),
 
   /* Keymap _QFL: Qwerty Function Layer
    */
@@ -49,11 +49,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Keymap _DPL: Dvorak Programmer Layer
    */
 [_DPL] = LAYOUT_65_iso_blocker(
-  KC_DLR,    KC_PLUS,  KC_LBRC,  KC_LCBR,  KC_LPRN,  KC_AMPR,  KC_EQL,   KC_RPRN,  KC_RCBR,  KC_RBRC,  KC_ASTR,  KC_EXLM,   KC_HASH,   KC_BSPC,  KC_DEL,
-  KC_TAB,    KC_SCLN,  KC_COMM,  KC_DOT,   KC_P,     KC_Y,     KC_F,     KC_G,     KC_C,     KC_R,     KC_L,     KC_SLSH,   KC_AT,               KC_PGUP,
-  KC_ESC,    KC_A,     KC_O,     KC_E,     KC_U,     KC_I,     KC_D,     KC_H,     KC_T,     KC_N,     KC_S,     KC_MINS,   KC_BSLS,   KC_ENT,   KC_PGDN,
-  MO(_DPSL), KC_BSPC,  KC_QUOT,  KC_Q,     KC_J,     KC_K,     KC_X,     KC_B,     KC_M,     KC_W,     KC_V,     KC_Z,      MO(_DPSL), KC_UP,    KC_RCTL,
-  KC_LCTL,   KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_RALT,  MO(_DPFL), KC_LEFT,   KC_DOWN,  KC_RGHT),
+  KC_DLR,    KC_PLUS,    KC_LBRC,  KC_LCBR,  KC_LPRN,  KC_AMPR,  KC_EQL,   KC_RPRN,  KC_RCBR,  KC_RBRC,  KC_ASTR,  KC_EXLM,   KC_HASH,   KC_BSPC,  KC_DEL,
+  KC_TAB,    KC_SCLN,    KC_COMM,  KC_DOT,   KC_P,     KC_Y,     KC_F,     KC_G,     KC_C,     KC_R,     KC_L,     KC_SLSH,   KC_AT,               KC_PGUP,
+  KC_ESC,    KC_A,       KC_O,     KC_E,     KC_U,     KC_I,     KC_D,     KC_H,     KC_T,     KC_N,     KC_S,     KC_MINS,   KC_BSLS,   KC_ENT,   KC_PGDN,
+  MO(_DPSL), MO(_DPFL),  KC_QUOT,  KC_Q,     KC_J,     KC_K,     KC_X,     KC_B,     KC_M,     KC_W,     KC_V,     KC_Z,      MO(_DPSL), KC_UP,    KC_RCTL,
+  KC_LCTL,   KC_LGUI,    KC_LALT,                                KC_SPC,                                 KC_RALT,  MO(_DPFL), KC_LEFT,   KC_DOWN,  KC_RGHT),
 
   /* Keymap _DPL: Dvorak Programmer Shift Layer
    */
